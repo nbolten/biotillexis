1c253c676277637cb71642b2d318813ac53d3ea8
Nick and Leli <bolten@uw.edu>
2013-03-23 04:28:49 +0000

    pep8

--- track_color.py
+++ track_color.py
@@ -1,13 +1,13 @@
-#PINK = H: 153.0       S: 192.0        V: 93.0
-#BLUE ON WHITE = H: 105.0       S: 240.0        V: 154.0
-#BLACK ON WHITE = 13, 80, 60
-#GREEN ON WHITE = 85, 230, 160
-
+#OpenCV
 import cv
 import cv2
+
+#Built-in Library
 import numpy as np
 
 
+#Function: Takes in an image and color parameter.
+#Returns list of coordinates of objects of that color in the image.
 def track_color(img, color):
     # Green settings
     GREEN_MIN = np.array([42, 186, 99], np.uint8)
@@ -26,30 +26,30 @@ def track_color(img, color):
     else:
         raise Exception('Invalid color parameter')
 
-#    img = cv2.imread('cam_home.jpeg')
-
-    cv.Smooth(cv.fromarray(img), cv.fromarray(img), cv.CV_BLUR, 3);
+    #Filter out image noise
+    cv.Smooth(cv.fromarray(img), cv.fromarray(img), cv.CV_BLUR, 3)
+    #Convert image to hsv
     hsv_img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    #Check for colors withing range of input color
     frame_threshed = cv2.inRange(hsv_img, color_min, color_max)
-
-#    cv2.imwrite('output_thresholded.jpeg', frame_threshed)
+    #Form contours around elements of chosen color
     contours, hierarchy = cv2.findContours(frame_threshed,
                                            cv2.RETR_TREE,
                                            cv2.CHAIN_APPROX_SIMPLE)
     coord_list = []
     for cnt in contours:
-	 area = cv2.contourArea(cnt)
-	 if area>500:
-	 	M = cv2.moments(cnt)
-	        centroid_x = int(M['m10']/M['m00'])
-	        centroid_y = int(M['m01']/M['m00'])
-	        coord_list.append((centroid_x,centroid_y))
-
+        area = cv2.contourArea(cnt)
+        #Discard contours of elements that are too small
+        if area > 500:
+            M = cv2.moments(cnt)
+            centroid_x = int(M['m10']/M['m00'])
+            centroid_y = int(M['m01']/M['m00'])
+            coord_list.append((centroid_x, centroid_y))
+    #Draw final contours on image
     cv2.drawContours(img, contours, -1, (0, 255, 0), 5)
-
+    #Return contour coordinates and final image
     return coord_list, img
 
 if __name__ == '__main__':
     track_color('green')
     #track_color('pink')
-
